534cfdbbe17bdb7d9be7da886dd490a233dee6be
Aliyu Abdulrazak <106888664+Razak002@users.noreply.github.com>
2023-11-19 00:10:08 +0100

    Update Navbar.js
    nav bar updated

--- app/componenets/Navbar.js
+++ app/componenets/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <div className='flex justify-between items-center w-full'>
                     <Link href='/'>
                         <h1 className='text-2xl font-medium'>
-                            Razak's <span className='text-green-500'>Blog</span>
+                        Razak&apos;s <span className='text-green-500'>Blog</span>
                         </h1>
                     </Link>
                     <Themebutton />
